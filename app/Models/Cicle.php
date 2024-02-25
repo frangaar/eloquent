be3d73d3c97f45062d3540148e4ae789cabd5449
Fran Garcia <francisco.garcia.arroyo@gmail.com>
2024-02-25 21:31:21 +0100

    Modelos

---
+++ app/Models/Cicle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cicle extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cicles';
+    protected $primaryKey = 'id';
+    //public $incrementing = false
+    //protected $keyType = 'string';
+    public $timestamps = false;
+
+}
